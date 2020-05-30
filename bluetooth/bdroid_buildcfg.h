d4036cffe766ef9fdc7a860b26b5fa242ec2b652
Nick C <ctwoon@suicide.today>
2020-05-30 12:50:04 +0300

    rosy: update bdroid_buildcfg values from caf
    https: //source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/bluetooth-commonsys-intf/tree/build/qva/config/bdroid_buildcfg.h?h=LA.UM.8.6.2.r1-08400-89xx.0
    Change-Id: I5a95bec3db32eb4f6f83f33087612cf43294fc5b
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,14 +19,14 @@
 
 #define BTM_DEF_LOCAL_NAME   "Redmi 5"
 #define MAX_ACL_CONNECTIONS    16
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
+#define GATT_MAX_PHY_CHANNEL  10
 
 #define BT_CLEAN_TURN_ON_DISABLED 1
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
-/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
-#define AVDT_NUM_SEPS 12
+#define AVDT_NUM_SEPS 35
 
 #define BTIF_HF_WBS_PREFERRED TRUE
 #endif
