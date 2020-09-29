2114427925152368eb953e7f8dd21c0f80383c2d
Sonal Singh <sonal.singh.19993@gmail.com>
2020-09-29 22:32:34 +0530

    rosy: Enable real time lockscreen charging current values
    Signed-off-by: Ray <vincentrayananda@gmail.com>
    Change-Id: I1881823fc4ea61a3dc973aa8937c2704c561abe8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,9 @@ USE_OPENGL_RENDERER := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Filesystem
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
