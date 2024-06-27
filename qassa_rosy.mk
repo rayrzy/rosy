bf7f2b2c09bb974140485e676ee6d9b94d2c97f4
rayrzy <vincentrayananda@gmail.com>
2024-06-28 00:15:22 +0700

    rosy: QASSA-ify
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- qassa_rosy.mk
+++ qassa_rosy.mk
@@ -9,24 +9,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common QASSA stuff
+$(call inherit-product, vendor/qassa/config/common_full_phone.mk)
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_GAPPS_ARCH := arm64
+USE_PIXEL_CHARGING := true
 
 # Inherit from rosy device
-$(call inherit-product, device/xiaomi/rosy/device.mk)
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_rosy
 PRODUCT_DEVICE := rosy
+PRODUCT_NAME := qassa_rosy
+BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
 PRODUCT_MANUFACTURER := Xiaomi
-
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_BOOTANIMATION_HALF_RES := true
-
-# GApps
-TARGET_GAPPS_ARCH := arm64
+TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
